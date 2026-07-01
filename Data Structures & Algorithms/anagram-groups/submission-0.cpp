5f911a4e739f271bbd157d5d7f1c6611cfe0f20b
Divya Sravani Sankarasetty <96373392+SDivyaSravani@users.noreply.github.com>
2026-07-01 13:26:41 +0530

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,vector<string>>mpp;
+        for(auto s:strs){
+            string key = s;
+            sort(key.begin(),key.end());
+            mpp[key].push_back(s);
+        }
+
+        vector<vector<string>> result;
+
+        for(auto r:mpp){
+            result.push_back(r.second);
+        }
+        return result;
+    }
+};
