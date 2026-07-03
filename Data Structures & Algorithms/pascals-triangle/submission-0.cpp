6b7aa9203bebc6ffc7bb75bf773215484857c555
Divya Sravani Sankarasetty <96373392+SDivyaSravani@users.noreply.github.com>
2026-07-03 10:13:10 +0530

    Add: pascals-triangle - submission-0

---
+++ Data Structures & Algorithms/pascals-triangle/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>>arr;
+        for(int i=0;i<numRows;i++){
+            vector<int>a;
+            for(int j=0;j<=i;j++){
+                if(j==0 || j==i){
+                    a.push_back(1);
+                }else{
+                    a.push_back(arr[i-1][j-1]+arr[i-1][j]);
+                }
+            }
+            arr.push_back(a);
+        }
+        return arr;
+    }
+};
