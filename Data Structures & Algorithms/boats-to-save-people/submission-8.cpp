de4ae3c49e5a47640c18968a49f28d86bd487f0e
Divya Sravani Sankarasetty <96373392+SDivyaSravani@users.noreply.github.com>
2026-07-27 11:38:08 +0530

    Add: boats-to-save-people - submission-8

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-8.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        int n = people.size();
+        sort(people.begin(),people.end());
+        int i = 0;
+        int j = n-1;
+        int cnt = 0;
+        while(i<j){
+            if((people[i]+people[j]) <= limit){
+                cnt = cnt+1;
+                i++;
+                j--;
+            }else{
+                j--;
+            }
+        }
+        return n-cnt;
+    }
+};
