4885ba8aa285966621de32fa0d5be31b5f87d43b
Divya Sravani Sankarasetty <96373392+SDivyaSravani@users.noreply.github.com>
2026-08-07 10:17:02 +0530

    Bulk sync: 95 submissions

--- Data Structures & Algorithms/three-integer-sum/submission-6.cpp
+++ Data Structures & Algorithms/three-integer-sum/submission-6.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         sort(nums.begin(),nums.end());
+        vector<vector<int>>res;
         int n = nums.size();
-        vector<vector<int>>result;
         for(int i=0;i<n;i++){
             if(i>0 && nums[i]==nums[i-1]){
                 continue;
@@ -14,23 +14,21 @@ public:
                 long long s = nums[i]+nums[j];
                 s += nums[k];
                 if(s==0){
-                    result.push_back({nums[i],nums[j],nums[k]});
+                    res.push_back({nums[i],nums[j],nums[k]});
                     j++;
                     k--;
-                    while(j<k && nums[j]==nums[j-1]){
-                        j++;
-                    }
-                    while(j<k && nums[k]==nums[k+1]){
-                        k--;
-                    }
+                    while(j>k && nums[j]==nums[j-1]){j++;} 
+                    while(j<k && nums[k]==nums[k+1]) {k--;}
                 }
-                else if(s<0){
-                    j++;
-                }else{
+                
+                if(s>0){
                     k--;
+                }
+                if(s<0){
+                    j++;
                 }
             }
         }
-        return result;
+        return res;
     }
 };
---
+++ Data Structures & Algorithms/two-integer-sum/submission-6.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        unordered_map<int,int>mpp;
+        mpp.reserve(n);
+        for(int i=0;i<n;i++){
+            int sum = target - nums[i];
+            if(mpp.find(sum)!=mpp.end()) {
+                return {mpp[sum],i};
+            }
+            mpp[nums[i]] = i;
+        }
+        return {-1,-1};
+    }
+};
