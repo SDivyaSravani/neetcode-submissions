bac562e7a44ac47eb322444d8c71fd56bf144d5b
Divya Sravani Sankarasetty <96373392+SDivyaSravani@users.noreply.github.com>
2026-08-18 18:44:31 +0530

    Add: first-missing-positive - submission-5

---
+++ Data Structures & Algorithms/first-missing-positive/submission-5.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int n = nums.size();
+        unordered_set<int>mpp;
+        for(int i:nums){
+            if(i>0){
+                mpp.insert(i);
+            }
+        }
+
+        for(int i=1;i<=n;i++){
+            if(mpp.find(i)==mpp.end()){
+                return i;
+            }
+        }
+        return n+1;
+    }
+};
