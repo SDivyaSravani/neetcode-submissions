ec3360262fe69d08dd4cf99199c36d9582eb4f5d
Divya Sravani Sankarasetty <96373392+SDivyaSravani@users.noreply.github.com>
2026-08-25 09:45:53 +0530

    Add: eating-bananas - submission-5

---
+++ Data Structures & Algorithms/eating-bananas/submission-5.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+
+    long long Total_Hrs(vector<int>& piles, int h,int mid){
+        int n = piles.size();
+        long long totalHrs = 0;
+        for(int i=0;i<n;i++){
+            totalHrs += ceil(double(piles[i])/double(mid));
+        }
+        return totalHrs;
+    }
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int high = *max_element(piles.begin(),piles.end());
+        int low = 1;
+        int ans = high;
+        while(low<=high){
+            long long mid = (low+high)/2;
+            long long total = Total_Hrs(piles,h,mid);
+            if(total<=h){
+                ans = mid;
+                high = mid-1;
+            }else{
+                low = mid+1;
+            }
+        }
+        return ans;
+    }
+};
