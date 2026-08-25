2c5a211060dd13f42af1256c2eb68e5b0ebe5b9b
Divya Sravani Sankarasetty <96373392+SDivyaSravani@users.noreply.github.com>
2026-08-25 11:04:53 +0530

    Add: intersection-of-two-arrays - submission-4

---
+++ Data Structures & Algorithms/intersection-of-two-arrays/submission-4.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums1.size();
+        unordered_set<int>s(nums1.begin(),nums1.end());
+        unordered_set<int>res;
+        for(auto num:nums2){
+            if(s.count(num)){
+                res.insert(num);
+                s.erase(num);
+            }
+        }
+        vector<int> res1(res.begin(),res.end());
+        return res1;
+    }
+};
